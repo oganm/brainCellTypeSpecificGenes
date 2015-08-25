138d2eb8dd61504d34ed592820bfb36f3f3b7313
OganM <ogan.mancarcii@gmail.com>
2015-08-25 14:33:35 -0700

    hmm spaces and shit?

--- R/estimate.R
+++ R/estimate.R
@@ -14,7 +14,7 @@ fullEstimate = function(exprData, # expression data
                         geneColName, # collumn name in the expression data that contains gene names
                         groups, # a vector stating which groups each sample belongs to
                         outDir, # output directory for plots and tables
-                        seekConsensus=F, # for trimming genes that behave differently in different groups 
+                        seekConsensus=F, # for trimming genes that behave differently in different groups
                         groupRotations=F, # output rotations of individiual genes
                         outlierSampleRemove=T, # if T outliers in each sample is removed
                         geneTransform = function(x){mouse2human(x)$humanGene}, # function to use when translating gene names
@@ -34,7 +34,7 @@ fullEstimate = function(exprData, # expression data
                                  PC = PC)
     estimates$estimates = trimNAs(estimates$estimates)
     estimates$groups = trimNAs(estimates$groups)
-    
+
     if (groupRotations){
         groupRotations(exprData,
                        geneTransform=geneTransform,
@@ -43,12 +43,12 @@ fullEstimate = function(exprData, # expression data
                        groups=groups,
                        outDir=outDir)
     }
-    
+
     plotEstimates(estimates$estimates,estimates$groups,
                   paste0(outDir,
                          names(estimates$estimates),'.svg'),
                   pAdjMethod = pAdjMethod)
-    
+
 }
 
 
@@ -64,22 +64,22 @@ plotEstimates = function(estimates,groups,plotNames, sigTest =  wilcox.test,
                          sigTreshold = 0.05){
     toCreate = unique(dirname(plotNames))
     sapply(toCreate,dir.create,showWarnings = F,recursive=T)
-    
-    
+
+
     groupNames = as.character(unique(groups[[1]]))
-    
+
     if (typeof(estimates)!='list'){
         estimates = list(estimates)
     }
-    
+
     if (comparisons == 'all'){
         comparisons = combn(groupNames,2)
     }
-    
+
     # create p value lists for correction
     pList = matrix(data = NA, ncol = ncol(comparisons), nrow = len(estimates))
     for (i in 1:len(estimates)) {
-        
+
         for (j in 1:ncol(comparisons)){
             pList[i, j] = sigTest(estimates[[i]][groups[[i]] %in% comparisons[1,j]],
                                   estimates[[i]][groups[[i]] %in% comparisons[2,j]])$p.value
@@ -91,44 +91,44 @@ plotEstimates = function(estimates,groups,plotNames, sigTest =  wilcox.test,
             pList[,i] = p.adjust(pList[,i], pAdjMethod)
         }
     }
-    
+
     if (correction[1]=='all'){
         pList = matrix(p.adjust(cbind(pList,pList)),nrow = len(estimates))
     }
-    
-    
+
+
     # plotting of things
     for (i in 1:len(estimates)){
         frame = data.frame(PC1 = estimates[[i]], group = groups[[i]])
         windowUp = max((frame$PC1)) + 1
         windowDown = min((frame$PC1)) - 0.5
-        
+
         # prepare significance text
         sigText = apply(comparisons, 2, paste0, collapse = '/')
-        
+
         sigText = paste0(sigText,': ',sprintf('%.5f',pList[i,]),collapse = '\n')
-        
+
         lePlot = ggplot(frame,aes(x=group, y = PC1)) +
-            geom_violin( color="#C4C4C4", fill="#C4C4C4") + 
-            geom_boxplot(width=0.1,fill = 'lightblue') + 
-            geom_point(size = 3) + 
-            ggtitle(names(estimates)[i]) + 
+            geom_violin( color="#C4C4C4", fill="#C4C4C4") +
+            geom_boxplot(width=0.1,fill = 'lightblue') +
+            geom_point(size = 3) +
+            ggtitle(names(estimates)[i]) +
             scale_y_continuous(limits=c(windowDown, windowUp),
                                name="Relative estimate of cell type amounts") +
-            theme_bw() + 
+            theme_bw() +
             theme(axis.text.x  = element_text(size=25),
                   axis.title.y = element_text(vjust=0.5, size=25),
                   axis.title.x = element_text(vjust=0.5, size=0) ,
                   title = element_text(vjust=0.5, size=25),
-                  axis.text.y = element_text(size = 13)) + 
+                  axis.text.y = element_text(size = 13)) +
             annotate('text', x = 0.1 , y = windowUp ,
                      label = sigText ,
                      hjust = 0, vjust=1, size = 4.5)
         (lePlot)
         ggsave(plotNames[i],width=8,height=8)
-        
+
     }
-    
+
 }
 
 
@@ -136,9 +136,9 @@ plotEstimates = function(estimates,groups,plotNames, sigTest =  wilcox.test,
 # estimates relative abundances of cell types based on the PC1s of gene expressions accross samples
 # controlBased is the name of the control group in 'groups'. if given, PC calculations will be
 # done on controls and rotations found will be applied to other samples.
-# groups variable should be provided if the plot is groupBased and/or if 
+# groups variable should be provided if the plot is groupBased and/or if
 # controlBased is set to a name of a group
-cellTypeEstimate = function(exprData, 
+cellTypeEstimate = function(exprData,
                             genes,
                             geneColName = 'Gene.Symbol',
                             # implement soon #46
@@ -146,9 +146,9 @@ cellTypeEstimate = function(exprData,
                             #validateRotation=NA,
                             outlierSampleRemove = T,
                             synonymTaxID = NULL, # do you want to add synonyms? no you don't don't touch this
-                            geneTransform = function(x){mouse2human(x)$humanGene}, 
+                            geneTransform = function(x){mouse2human(x)$humanGene},
                             groups = NA,
-                            controlBased = NULL, 
+                            controlBased = NULL,
                             tableOut = NULL,
                             indivGenePlot = NULL, # where should it plot individual gene expression plots.
                             seekConsensus = F,
@@ -164,12 +164,12 @@ cellTypeEstimate = function(exprData,
     }
     # if seeking consensus, check group based rotations
     if(seekConsensus){
-        groupRotations = groupRotations(exprData, genes, 
+        groupRotations = groupRotations(exprData, genes,
                                         geneColName, groups, outDir=NA,
                                         geneTransform = geneTransform,
                                         synonymTaxID = NA)
     }
-    
+
     estimateOut = vector(mode = 'list', length = len(genes))
     groupsOut = vector(mode = 'list', length = len(genes))
     rotations = vector(mode = 'list', length = len(genes))
@@ -181,8 +181,8 @@ cellTypeEstimate = function(exprData,
         if (!is.null(synonymTaxID)){
             genes[[i]] == unlist(geneSynonym(genes=genes[[i]],tax=synonymTaxID))
         }
-        
-        
+
+
         #remove non concenting genes (based on group) if is nested because there
         #will be no group rotations to look at if seekConsensus=F some redundancy
         # exists but oh well
@@ -191,7 +191,7 @@ cellTypeEstimate = function(exprData,
                 genes[[i]] = rownames(groupRotations[[i]][apply(groupRotations[[i]],1,function(x){all(x>0)}),])
             }
         }
-        
+
         relevantData = exprData[exprData[, geneColName] %in% genes[[i]],]
         # what to do if no genes from the list is found
         if (nrow(relevantData)==0){
@@ -199,15 +199,15 @@ cellTypeEstimate = function(exprData,
             groupsOut[[i]]=NA
             next
         }
-        
+
         rownames(relevantData) = relevantData[, geneColName]
         list[,relevantExpr] = sepExpr(relevantData)
-        
-        
+
+
         if (!is.null(indivGenePlot[1])){
             indivGenes = data.frame(melt(relevantExpr)[,2],rownames(relevantExpr))
             names(indivGenes) = c('expression', 'gene')
-            
+
             switch(plotType[1],
                    groupBased = {
                        indivGenes =  data.frame(indivGenes,group = groups)
@@ -215,39 +215,39 @@ cellTypeEstimate = function(exprData,
                    cummulative = {
                        indivGenes =  data.frame(indivGenes,group = '')
                    })
-            
+
             p = ggplot(indivGenes,aes(y = expression, x = group )) +
-                facet_wrap('gene') + 
+                facet_wrap('gene') +
                 geom_boxplot(fill = 'lightblue') +
                 theme(axis.text.x  = element_text( size=20),
                       axis.title.y = element_text(vjust=0.5, size=20),
-                      axis.title.x = element_text(vjust=0.5, size=0) , 
+                      axis.title.x = element_text(vjust=0.5, size=0) ,
                       title = element_text(vjust=0.5, size=20))+
                 scale_x_discrete(name = '')+
                 scale_y_discrete(name = 'log2 Expression')
             (p)
             ggsave(filename = indivGenePlot[i], width=8,height=8)
         }
-        
+
         # get rotations based on preferences
         if (is.null(controlBased)){
             pca = prcomp(t(relevantExpr), scale = T)
         } else{
             pca = prcomp(t(relevantExpr[groups %in% controlBased]), scale = T)
         }
-        
+
         # get the final rotations, make sure everything is positive
         pca$rotation = pca$rotation * ((sum(pca$rotation[,PC])<0)*(-2)+1)
         rotations[[i]] = pca$rotation
-        
+
         # output the rotation tables
         if (!is.null(tableOut[1])){
-            
+
             # add variation explained as a comment on top
             file.create(tableOut[i])
             fileConn = file(tableOut[i])
-            writeLines(paste0('# Variation explained: ', 
-                              paste(summary(pca)$importance[2,], collapse=' ')), 
+            writeLines(paste0('# Variation explained: ',
+                              paste(summary(pca)$importance[2,], collapse=' ')),
                        fileConn)
             close(fileConn)
             write.table(pca$rotation[,PC,drop=F],
@@ -255,8 +255,8 @@ cellTypeEstimate = function(exprData,
                         quote = F, row.names = T, col.names = F, sep='\t',
                         append = T)
         }
-        
-        
+
+
         pca$x = t(as.matrix(t(scale(t(relevantExpr))))) %*% as.matrix(pca$rotation)
         groupsOut[[i]] = groups
         # outlier removal
@@ -266,7 +266,7 @@ cellTypeEstimate = function(exprData,
             },simplify=F)
             names(groupData) = unique(groups)
             box = boxplot(groupData, plot = F)
-            # because of this part, sample names are important!!! uses them 
+            # because of this part, sample names are important!!! uses them
             # to match outliers
             groupsOut[[i]] = groups[!rownames(pca$x) %in% names(box$out)]
             pca$x = pca$x[!rownames(pca$x) %in% names(box$out),,drop=F]
@@ -277,7 +277,7 @@ cellTypeEstimate = function(exprData,
     names(groupsOut) = names(genes)
     names(rotations) = names(genes)
     output = list(estimates=estimateOut,groups=groupsOut, rotations  = rotations)
-    
+
     return(output)
 }
 
@@ -289,12 +289,12 @@ groupRotations = function(exprData, genes,geneColName, groups, outDir,
     if (typeof(genes)!='list'){
         genes = list(genes)
     }
-    
+
     allRotations = vector(mode = 'list', length=len(unique(genes)))
     for (i in 1:len(genes)){
-        
+
         rotations = vector(mode = 'list', length=len(unique(groups)))
-        
+
         if(!is.null(geneTransform)){
             genes[[i]] = geneTransform(genes[[i]])
         }
@@ -306,16 +306,16 @@ groupRotations = function(exprData, genes,geneColName, groups, outDir,
             allRotations[[i]]=NA
             next
         }
-        
+
         rownames(relevantData) = relevantData[, geneColName]
         list[,relevantExpr] = sepExpr(relevantData)
-        
+
         for (j in 1:len(unique(groups))){
             pca = prcomp(t(relevantExpr[groups %in% unique(groups)[j]]), scale = T)
             pca$rotation = pca$rotation * ((sum(pca$rotation[,1])<0)*(-2)+1)
             rotations[[j]] = pca$rotation[,1]
         }
-        
+
         rotations = as.data.frame(rotations)
         names(rotations) = unique(groups)
         allRotations[[i]] = rotations
