eb224412ccfa5a23776f5da55ad9abb7b08a2599
OganM <ogan.mancarcii@gmail.com>
2015-08-31 20:17:47 -0700

    soft file parser added, human metadata processing added

--- analysis/00.Preprocessing/00.downloadData.R
+++ analysis/00.Preprocessing/00.downloadData.R
@@ -16,6 +16,40 @@ getGemmaAnnotGoogle('GPL5175','data/GemmaAnnots/GPL5175')
 homoloGeneTarget = 'data/homogene.tsv'
 setUpHomologene()
 
-# get human data
-download.file('ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE46nnn/GSE46706/soft/GSE46706_family.soft.gz',destfile='data/GSE46706_family.soft.gz')
+# get human data ------------------------
+download.file('ftp://ftp.ncbi.nlm.nih.gov/geo/series/GSE46nnn/GSE46706/soft/GSE46706_family.soft.gz',
+              destfile='data/GSE46706_family.soft.gz')
 system('gunzip data/GSE46706_family.soft.gz')
+
+
+softData = softParse('data/GSE46706_family.soft')
+
+softData = softData[,c('!Sample_characteristics_ch1 = age at death (in years)', 
+                       '!Sample_characteristics_ch1 = ancestry',
+                       '!Sample_characteristics_ch1 = brain bank',
+                       '!Sample_characteristics_ch1 = brain ph',
+                       '!Sample_characteristics_ch1 = brain region',
+                       "!Sample_characteristics_ch1 = Cause of death",
+                       "!Sample_characteristics_ch1 = disease status",
+                       "!Sample_characteristics_ch1 = individual id",
+                       "!Sample_characteristics_ch1 = post-mortem interval (in hours)",
+                       "!Sample_characteristics_ch1 = rin",
+                       "!Sample_characteristics_ch1 = Sex",
+                       '!Sample_title')]
+
+colnames(softData) = c('deatAge',
+                       'ancestry',
+                       'brainBank',
+                       'pH',
+                       'brainRegion',
+                       'deathCause',
+                       'disease',
+                       'invidID',
+                       'postMortemInt',
+                       'rnaInt',
+                       'sex',
+                       'title')
+
+softData$GSM = rownames(softData)
+write.design(softData,file='data/GSE46706_meta')
+
