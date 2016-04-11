d5feae682c8ec252480a65d8847af26163386825
OganM <ogan.mancarcii@gmail.com>
2016-04-11 16:10:14 -0700

    changes to regionization. now it's a tree. changes to design file to add samples for shreejoy and accomodate changes

---
+++ R/regionHierarchy.R
@@ -0,0 +1,13 @@
+regionHierarchy = list(All = list(Cerebrum = 
+                                      list(Cortex = '',
+                                           BasalForebrain ='',
+                                           Striatum = '',
+                                           Amygdala ='',
+                                           Hippocampus = ''),
+                                  Subependymal = '',
+                                  Thalamus = '',
+                                  Brainstem = 
+                                      list(LocusCereuleus = ''),
+                                  Cerebellum = '',
+                                  SpinalCord ='')
+)
--- R/regionize.R
+++ R/regionize.R
@@ -1,33 +1,100 @@
-regionize = function(design,regionNames,groupNames){
-    regions =
-        trimNAs(
-            trimElement(
-                unique(
-                    unlist(
-                        strsplit(as.character(design[,regionNames]),',')))
-                ,c('ALL','All','all','Cerebrum'))) #S pecial names
-    regionBased = expand.grid(groupNames, regions)
-    regionGroups = vector(mode = 'list', length = nrow(regionBased))
-    names(regionGroups) = paste0(regionBased$Var2,'_',regionBased$Var1)
-    for (i in 1:nrow(regionBased)){
-        regionGroups[[i]] = design[,as.character(regionBased$Var1[i])]
+library(dplyr)
+library(lazyeval)
+library(magrittr)
+
+
+#' Create cell type annotations per region.
+#' @description For each brain region, based on a defined hierarchy, it choses which samples to take in
+#' @param design A data.frame containing the design information
+#' @param regionNames a string naming the collumn of the design table that indicates the region names
+#' @param groupNames a string vector naming  the collumns of the design table that indicates cell type naming schemes.
+#' @param regionHierarchy a list of lists that represents the region hierarchy.
+#' @return A list containing character vectors that annotates samples. If the sample will not be considered for the region
+#' it will be represented as an NA, otherwise it'll have the cell type name indicated in the region 
+regionize = function(design,regionNames,groupNames, regionHierarchy){
+    
+    regionsTree = regionHierarchy %>% unlist %>% names %>% strsplit(split='\\.') %>% unlist %>% unique
+    
+    # some non used samples have undefined regions, trim those since you don't use them anyway
+    regionsData = design[design[,groupNames]%>% apply(1,function(x){all(!is.na(x))}),
+                         regionNames] %>% strsplit(',') %>% unlist %>% trimNAs %>% unique
+   
+    # ensure that every region in the dataset is somewhere in the tree
+    assertthat::assert_that(len(regionsData[!(regionsData %in%  regionsTree)]) == 0)
+    
+    
+    # get the region names with their parents so children can inherit their parents sins
+    childrenRegions =  regionHierarchy %>%
+        unlist %>% 
+        names
         
-        # remove everything except the region and ALL labeled ones. for anything but cerebellum, add Cerebrum labelled ones as well
-        if (regionBased$Var2[i] == 'Cerebellum'){
-            regionGroups[[i]][!grepl(paste0('(^|,)((',regionBased$Var2[i],')|((A|a)(L|l)(l|l)))($|,)'),design[,regionNames])] = NA
-        } else {
-            # look for cerebrums
-            cerebrums = unique(regionGroups[[i]][grepl('(Cerebrum)',design[,regionNames])])
+    regions = childrenRegions %>% strsplit(split='\\.') %>% lapply(function(x){
+        sapply(1:len(x), function(i){
+            paste(x[1:i],collapse='.')
+        })
+        
+    }) %>% unlist %>% unique
+    
+    
+    regionBased = expand.grid(groupNames, regions)
+    names(regionBased) = c('groupName','region')
+    regionBased %<>% mutate(groupName = as.char(groupName)) %>% mutate(region = as.char(region))
+    
+    regionGroups = vector(mode = 'list', length = nrow(regionBased))
+    names(regionGroups) = paste0(regionBased$region,'_',regionBased$groupName)
+    
+    # have translation to short names
+    regions = data.frame(region = regions)  %>%
+        mutate(shortNames = region %>% as.char %>% strsplit(split='\\.') %>% lapply(function(x){x[len(x)]}) %>% unlist)
+    
+    # get a full name for all listed regions per sample (samples can have multiple regions if they are specifically 
+    # extracted from a lower level region but we want to use them for others)
+    regionList = design[,regionNames] %>% strsplit(',') %>% lapply(function(x){
+        as.char(regions$region[match(x,regions$shortNames)])
+    })
+    
+    # i is the region groups to assign
+    # j rotates over samples and decides whether or not to assign them to the region
+    for (i in 1:nrow(regionBased)){
+        regionGroups[[i]] = design[,as.character(regionBased$groupName[i])]
+        
+        # decide which samples should belong to the region
+        inRegion = sapply(1:len(regionList), function(j){
+            # get the ones that are directly listed in the region
+            inReg = regionBased$region[i] %in%  regionList[[j]]
             
-            # find which cerebrums are not represented in the region
-            cerebString = paste(cerebrums[!cerebrums %in% regionGroups[[i]][grepl(paste0('(^|,)((',regionBased$Var2[i],')|((A|a)(L|l)(l|l)))($|,)'),design[,regionNames])]],
-                                collapse = ')|(')
+            # get ones that are in children nodes this is done because a larger brain region will include cell types 
+            # taken from more specific brain regions
+            if (design[j,'RegionToParent']){                
+                inReg = inReg | (
+                    regionList[[j]] %>%
+                        grepl(paste0('^',regionBased$region[i]),  .) %>% any)
+            }
             
-            # add them as well (or not remove them as well) with all the rest of the region samples
-            regionGroups[[i]][(!grepl(paste0('(^|,)((',regionBased$Var2[i],')|((A|a)(L|l)(l|l)))($|,)'),design[,regionNames])
-                               & !(grepl(paste0('(',cerebString,')'),design[,as.character(regionBased$Var1[i])]) & grepl('Cerebrum',design[,regionNames])))] =  NA
+            # get ones that are in parent nodes. this is done because a smaller brain region will include cell types that
+            # are isolated from the larger region. do not add it if we have the specific cell type from the region
+            if(design[j,'RegionToChildren']){
+                # take a quick peek to make sure the cell type doesn't exist in a region closer to the core region
+                otherSamples = regionList[design[,regionBased$groupName[i]] == design[j,regionBased$groupName[i]]] %>% .[sapply(.,function(x){!is.null(x)})]
+                otherSamples %<>% unlist
+                override  = regionList[[j]] %>% sapply(function(x){
+                    gsub(paste0('\\Q',x,'\\E'),'', otherSamples[grepl(paste0('\\Q',x,'\\E'), otherSamples)]) %>%
+                        nchar %>% {.>0} %>% any
+                }) %>% all
+                if (!override){
+                toChild = regionBased$region[i] %>% 
+                    grepl(paste0('^',regexMerge( regionList[[j]], exact=TRUE)), .)
+                }
+                
+                
+                inReg = inReg | toChild
+            }
+            return(inReg)
             
-        } 
+        })
+        
+        regionGroups[[i]][!inRegion] =  NA
     }
+    names(regionGroups) = gsub(pattern='^.*\\.','',names(regionGroups))
     return(regionGroups)
 }
