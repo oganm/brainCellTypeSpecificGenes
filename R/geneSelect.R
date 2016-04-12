020215b10045f06b68b3b139cbd81040b692aa02
OganM <ogan.mancarcii@gmail.com>
2016-04-11 17:18:44 -0700

    geneSelect edited to use the region tree

--- R/geneSelect.R
+++ R/geneSelect.R
@@ -17,6 +17,7 @@ geneSelect = function(designLoc,
                       replicates = 'originalIndex',
                       foldChangeThresh = 10,
                       minimumExpression = 8){
+    source('R/regionHierarchy.R')
     # so that I wont fry my laptop
     if (detectCores()<cores){ 
         cores = detectCores()
@@ -106,42 +107,8 @@ geneSelect = function(designLoc,
         noReg = T
     }
     
-    # deal with region stuff ----
-#     regions =
-#         trimNAs(
-#             trimElement(
-#                 unique(
-#                     unlist(
-#                         strsplit(as.character(design[,regionNames]),',')))
-#                 ,c('ALL','All','all','Cerebrum'))) #S pecial names
-#     regionBased = expand.grid(groupNames, regions)
-#     regionGroups = vector(mode = 'list', length = nrow(regionBased))
-#     names(regionGroups) = paste0(regionBased$Var2,'_',regionBased$Var1)
-#     
-#     
-#     for (i in 1:nrow(regionBased)){
-#         regionGroups[[i]] = design[,as.character(regionBased$Var1[i])]
-#         
-#         # remove everything except the region and ALL labeled ones. for anything but cerebellum, add Cerebrum labelled ones as well
-#         if (regionBased$Var2[i] == 'Cerebellum'){
-#             regionGroups[[i]][!grepl(paste0('(^|,)((',regionBased$Var2[i],')|((A|a)(L|l)(l|l)))($|,)'),design[,regionNames])] = NA
-#         } else {
-#             # look for cerebrums
-#             cerebrums = unique(regionGroups[[i]][grepl('(Cerebrum)',design[,regionNames])])
-#             
-#             # find which cerebrums are not represented in the region
-#             cerebString = paste(cerebrums[!cerebrums %in% regionGroups[[i]][grepl(paste0('(^|,)((',regionBased$Var2[i],')|((A|a)(L|l)(l|l)))($|,)'),design[,regionNames])]],
-#                                 collapse = ')|(')
-#             
-#             # add them as well (or not remove them as well) with all the rest of the region samples
-#             regionGroups[[i]][(!grepl(paste0('(^|,)((',regionBased$Var2[i],')|((A|a)(L|l)(l|l)))($|,)'),design[,regionNames])
-#                                & !(grepl(paste0('(',cerebString,')'),design[,as.character(regionBased$Var1[i])]) & grepl('Cerebrum',design[,regionNames])))] =  NA
-#             
-#         }
-#         
-#         
-#     }
-    regionGroups = regionize(design,regionNames,groupNames)
+
+    regionGroups = regionize(design,regionNames,groupNames,regionHierarchy)
     # concatanate new region based groups to design and to groupNames so they'll be processed normally
     if (!noReg){
         design = cbind(design,regionGroups)
