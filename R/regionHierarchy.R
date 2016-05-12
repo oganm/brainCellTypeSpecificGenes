678c94e072c256554b69a8b18302d0b611092803
OganM <ogan.mancarcii@gmail.com>
2016-05-12 14:57:59 -0700

    regionHierarchy: transaltable into data.tree for plotting purposes

--- R/regionHierarchy.R
+++ R/regionHierarchy.R
@@ -12,3 +12,28 @@ regionHierarchy = list(All = list(Cerebrum =
                                   Cerebellum = '',
                                   SpinalCord ='')
 )
+
+
+library(data.tree)
+# to translate this into a data tree
+listToDataTree = function(list){
+    members = list %>% unlist %>% names
+    # they have to have a commont root
+    rootName = members[[1]] %>% strsplit(split='[.]') %>% {.[[1]][1]}
+    teval(paste0(rootName, 
+                 " = Node$new(members[[1]] %>% strsplit(split='[.]') %>% {.[[1]][1]})"),
+          envir = parent.frame())
+    for (x in members){
+        newNodes = x %>% strsplit(split='[.]') %>% {.[[1]]}
+        for (i in 2:len(newNodes)){
+            if(!is.null(teval(rootName)$FindNode(newNodes[i]))){
+                next
+            }
+            teval(paste0(newNodes[i], ' = ',newNodes[i-1],'$AddChild("',newNodes[i],'")'),
+                  envir = parent.frame())
+            
+        }
+    }
+    
+    return(teval(rootName))
+}
